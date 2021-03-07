7803b562be332b5217cc6e66cbc6a8f72b1784db
adi-23 <vallinarasimhaswamy.k19@iiits.in>
2021-03-07 23:29:00 +0530

    added login.html

---
+++ project35/project35/users/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,include
+import django.contrib.auth.urls
+from users import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/',views.home,name="home"),    
+   # path('register/',views.register,name='register'),
+    path('login/',views.login,name='login'),
+    path('users/',include('django.contri.auth.urls')),
+    
+]
--- project35/project35/users/views.py
+++ project35/project35/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
+ 
 # Create your views here.
+def home(request):
+    return HttpResponse("homepage")
+
+
+def login(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        if User.objects.filter(username=username,password=password).exists():
+            redirect('home/')
+        else:
+            return HttpResponse("INVALIED RESPONSE")
+    else:
+     return render(request,'users/login.html')   
+        
+        
+    
+
+
