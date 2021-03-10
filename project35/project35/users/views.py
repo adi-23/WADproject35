90732e281712af1dfbb22083cb7c17b48da30d41
adi-23 <vallinarasimhaswamy.k19@iiits.in>
2021-03-11 00:06:28 +0530

    samplecustomauthentication

--- project35/project35/users/views.py
+++ project35/project35/users/views.py
@@ -9,20 +9,3 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return HttpResponse("homepage")
-
-
-def login(request):
-    if request.method == 'POST':
-        username=request.POST['username']
-        password=request.POST['password']
-        if User.objects.filter(username=username,password=password).exists():
-            redirect('home/')
-        else:
-            return HttpResponse("INVALIED RESPONSE")
-    else:
-     return render(request,'users/login.html')   
-        
-        
-    
-
-
