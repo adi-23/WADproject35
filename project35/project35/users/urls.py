7e76532c94ee67423bfc745113d379680cadadc5
adi-23 <vallinarasimhaswamy.k19@iiits.in>
2021-03-11 16:43:05 +0530

    addedchanges

--- project35/project35/users/urls.py
+++ project35/project35/users/urls.py
@@ -5,7 +5,7 @@ from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',views.home,name="home"),    
+   # path('home/',views.home,name="home"),    
    # path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
     path('users/',include('django.contri.auth.urls')),
