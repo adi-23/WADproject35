7153b3dd52a460870f5732b884cab8e1b8fdd17d
Madhesh-Medasani <the.slayer.yt07@gmail.com>
2021-03-17 23:49:38 +0530

    Added close buttons for forms

--- project35/hotels/views.py
+++ project35/hotels/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from .models import Place,Hotel
 # Create your views here.
 def hotels(request):
-    placeid = Place.objects.filter(place_name='palakollu')
-    hotels_info= Hotel.objects.filter(hotel_place = placeid.get(id=2))
+    iid=0
+    for place in Place.objects.all():
+        if place.place_name=='guntur':
+            iid=place.id
+            break
+
+    hotels_info= Hotel.objects.filter(hotel_place=iid)
     context={'hotelsinfo': hotels_info, }
     return render(request,"hotels/base.html",context)
