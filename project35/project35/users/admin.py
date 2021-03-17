5252717df4a101bb9288de321a61b18e29f3bf73
adi-23 <56594303+adi-23@users.noreply.github.com>
2021-03-17 11:40:57 +0530

    Delete project35 directory

--- project35/project35/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User,Serviceusers,Serviceproviders
-# Register your models here.
-admin.site.register('User')
-admin.site.register('Serviceusers')
-
-admin.site.register('Serviceproviders')
