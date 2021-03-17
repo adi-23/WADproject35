64e7cf6b50b330f5d5661c46697daf63d24a9290
adi-23 <vallinarasimhaswamy.k19@iiits.in>
2021-03-17 16:39:17 +0530

    addedmodelstohotels

--- project35/hotels/models.py
+++ project35/hotels/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    place_name=models.CharField(max_length=30,unique=True)
+
+class Hotel(models.Model):
+    hotel_name          =models.CharField(max_length=50)
+    hotel_address       =models.CharField(max_length=100)
+    hotel_hasACrooms    =models.BooleanField(default=False)
+    hotel_place         =models.ForeignKey(Place,on_delete=models.CASCADE)
+    hotel_contactinfo   =models.CharField(max_length=10)
+    
