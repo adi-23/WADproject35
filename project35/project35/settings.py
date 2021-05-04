837a1f8103aeba36ce2d440aa9410d3101b3b89c
Lilly1030 <lillykumari.b19@iiits.in>
2021-05-04 19:17:43 +0530

    relocated homepage pics to media

--- project35/project35/settings.py
+++ project35/project35/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'users',
         'USER': 'root',
-        'PASSWORD': 'Aditya@177105',
+        'PASSWORD': 'Lilly1030',
         'HOST': 'localhost',
         'PORT': '3306',
 
